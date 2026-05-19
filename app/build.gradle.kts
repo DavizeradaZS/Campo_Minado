984d7d807c6f2906db4ae532d5fb6dc3d8082877
MarcusSampaio <marcuss.sampaioo@gmail.com>
2026-05-19 19:52:05 -0300

    update - add das dependencias para o retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,12 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation("androidx.fragment:fragment-ktx:1.6.2")
+
+    // Retrofit e Gson
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Coroutines para chamadas assíncronas
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 }
